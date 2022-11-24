ab26f4f15d26ec0c155a8d55b4fd341078eef334
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-24 11:38:32 +0300

    commit tree 1cf4765a258ea14526aecb110164947df605e02e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
-  def index
-    items = Item.all
-    render json: items, include: :user
+  rescue_from ActiveRecord::RecordNotFound, with: :unfound_response
+    def index
+      if params[:user_id]
+        user = User.find(params[:user_id])
+        items = user.items
+      else
+      items = Item.all
+      end
+      render json: items, include: :user
+    end
+  
+    def show
+      item = Item.find(params[:id])
+      render json: item, status: :ok
+    end
+  
+    def create
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      render json: item, status: :created
+    end
+  
+    private
+    def unfound_response
+    render json: {error: "Item not found"}, status: :not_found
+    end
+  
+    def item_params
+    params.permit(:name, :description, :price)
+    end
+    
   end
-
-end
